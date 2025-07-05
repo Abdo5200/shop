f3899c239670b6295be54bc10ac3be61557ef47a
Abdo5200 <abdelrahman.mamdouh2200@gmail.com>
2025-07-05 23:07:42 +0300

    added createdAt and updatedAt

--- models/product.js
+++ models/product.js
@@ -1,26 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const productSchema = new Schema({
-  title: {
-    type: String,
-    rquired: true,
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  price: {
-    type: Number,
-    rquired: true,
-  },
-  description: {
-    type: String,
-    rquired: true,
-  },
-  imageUrl: {
-    type: String,
-    rquired: true,
-  },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-});
+  {
+    timestamps: true, // This automatically adds createdAt and updatedAt fields
+  }
+);
 module.exports = mongoose.model("Product", productSchema);
