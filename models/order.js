1874fb8a8a7cfa7f21748555280af0d7b5f42484
Abdo5200 <abdelrahman.mamdouh2200@gmail.com>
2025-07-05 23:11:37 +0300

    order date

--- models/order.js
+++ models/order.js
@@ -1,18 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const orderSchema = new Schema({
-  user: {
-    userId: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
+const orderSchema = new Schema(
+  {
+    user: {
+      userId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+      },
     },
+    products: [
+      {
+        product: { type: Object, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
   },
-  products: [
-    {
-      product: { type: Object, required: true },
-      quantity: { type: Number, required: true },
-    },
-  ],
-});
+  {
+    timestamps: true, // This automatically adds createdAt and updatedAt fields
+  }
+);
 module.exports = mongoose.model("Order", orderSchema);
